e10507c4251ccb920d42375a292ce886479ac497
AMAN <antilaman3113@gmail.com>
2022-12-20 16:32:12 +0530

    init: used Api and useEffect for memes Array

---
+++ Meme generator/Src/App.js
@@ -0,0 +1,11 @@
+import Header from "./Components/Header";
+import Body from "./Components/Body.js";
+
+export default function App() {
+  return (
+    <div className="main-container">
+      <Header />
+      <Body />
+    </div>
+  );
+}
---
+++ Meme generator/Src/index.js
@@ -0,0 +1,5 @@
+import ReactDOm from "react-dom/client";
+import App from "./App.js";
+
+const Root = ReactDOm.createRoot(document.getElementById("root"));
+Root.render(<App />);
